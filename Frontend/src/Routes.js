7607a6191add31ef735e9777011ea5b56eb17b5f
japerry911 <jack.e.perry@outlook.com>
2020-04-15 15:30:17 -0600

    Initial pages created and routing set up

---
+++ Frontend/src/Routes.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Services from './pages/Services/Services';
+import OurWork from './pages/OurWork/OurWork';
+import Safety from './pages/Safety/Safety';
+import Shop from './pages/Shop/Shop';
+import News from './pages/News/News';
+import Careers from './pages/Careers/Careers';
+import ContactUs from './pages/ContactUs/ContactUs';
+
+export default () => (
+    <Switch>
+        <Route 
+            exact 
+            path='/'
+            component={Home}
+        />
+
+        <Route
+            exact
+            path='/about'
+            component={About}
+        />
+
+        <Route 
+            exact 
+            path='/services'
+            component={Services}
+        />
+
+        <Route
+            exact 
+            path='/our-work'
+            component={OurWork}
+        />
+
+        <Route 
+            exact
+            path='/safety'
+            component={Safety}
+        />
+
+        <Route 
+            exact
+            path='/shop'
+            component={Shop}
+        />
+
+        <Route 
+            exact 
+            path='/news'
+            component={News}
+        />
+
+        <Route 
+            exact 
+            path='/careers'
+            component={Careers}
+        />
+
+        <Route 
+            exact 
+            path='/contact-us'
+            compoennt={ContactUs}
+        />
+    </Switch>
+);
