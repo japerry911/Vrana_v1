657cb3ac0eb74b90e5c9c3c6a269bb3bea043eb4
japerry911 <jack.e.perry@outlook.com>
2020-04-15 09:37:27 -0600

    Finished basic Header

--- Frontend/src/components/Header/Header.js
+++ Frontend/src/components/Header/Header.js
@@ -4,8 +4,20 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header-div'>
-            <label className='description-label'>Call us: (402 733-5200</label>
-            <label className='description-label'>Email us: info@vrana.com</label>
+            <div className='sub-header-div'>
+                <img 
+                    src={require('../../assets/imgs/PhoneIcon.png')} 
+                    className='icon-image'
+                />
+                <label className='description-label'>Call us: (402 733-5200</label>
+            </div>
+            <div className='sub-header-div'>
+                <img
+                    src={require('../../assets/imgs/EmailIcon.png')}
+                    className='icon-image'
+                />
+                <label className='description-label'>Email us: info@vrana.com</label>
+            </div>
         </div>
     );
 };
