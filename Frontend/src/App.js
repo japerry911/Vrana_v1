92f4c863e5c25ee678b31d2f251b4d5a7786f840
japerry911 <jack.e.perry@outlook.com>
2020-04-15 17:39:54 -0600

    set up theme/themeprovider, starting on drawer/nav/menu

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,33 @@
+import React from 'react';
 import './App.css';
 import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import Routes from './Routes';
+
+let theme = createMuiTheme({
+  colors: {
+    vranaRed: '#CD2126',
+    textWhite: '#FCFEFF',
+    nonTextWhite: '#FEF7F7',
+    greyText: '#E3E2E2',
+    nonTextGrey: '#4B4A4A'
+  },
+  fonts: {
+    avenirItalicize: 'Avenir-Italicize',
+    avenirBold: 'Avenir-Bold',
+    avenirMedium: 'Avenir-Medium',
+    avenir: 'Avenir'
+  }
+});
+
+theme = responsiveFontSizes(theme);
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </div>
   );
 }
--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Home.css';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        color: theme.colors.vranaRed,
+        fontFamily: theme.fonts.avenirItalicize
+    }
+}));
 
 const Home = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div style={{backgroundColor: 'black'}}>
+            <Typography variant='h1' className={classes.root}>Home</Typography>
         </div>
     );
 };
