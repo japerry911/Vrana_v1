eefa66b0e201540e5c323b048b7cfc6b0bd565ea
japerry911 <jack.e.perry@outlook.com>
2020-04-18 17:58:05 -0600

    draft of top cards done

--- Frontend/src/pages/Services/ServicesStyles.js
+++ Frontend/src/pages/Services/ServicesStyles.js
@@ -1,3 +1,41 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles(theme => ({}));
+export const useStyles = makeStyles(theme => ({
+    redHeaderTextStyle: {
+        fontFamily: 'Avenir-Bold',
+        color: theme.colors.vranaRed,
+        display: 'inline'
+    },
+    darkGreyHeaderStyle: {
+        fontFamily: 'Avenir-Bold',
+        color: theme.colors.darkGrey
+    },
+    whiteContainerStyle: {
+        backgroundColor: theme.colors.nonTextWhite,
+        paddingBottom: '5em',
+        paddingTop: '2em'
+    },
+    gridItemCardStyle: {
+        paddingTop: '3em'
+    },
+    snapshotImageStyle: {
+        borderRadius: 10
+    },
+    medGreyHeaderStyle: {
+        fontFamily: 'Avenir-Medium',
+        color: theme.colors.darkGrey,
+        paddingTop: '1em'
+    },
+    darkGreyBodyStyle: {
+        fontFamily: 'Avenir',
+        color: theme.colors.darkGrey
+    },
+    redBodyStyle: {
+        fontFamily: 'Avenir-medium',
+        color: theme.colors.vranaRed
+    },
+    cardTextDivStyle: {
+        width: '50%', 
+        textAlign: 'left'
+    }
+}));
