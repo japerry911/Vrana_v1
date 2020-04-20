8352ab951a865b7e46eb5f787a786854e2fa36c7
japerry911 <jack.e.perry@outlook.com>
2020-04-20 14:35:58 -0600

    initial boilerplate redux integeration setup - useEffect added to dispatch getProjects()

--- Frontend/src/pages/OurWork/OurWork.js
+++ Frontend/src/pages/OurWork/OurWork.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import HorizontalTabs from '../../components/HorizontalTabs/HoritzontalTabs';
 import Footer from '../../components/Footer/Footer';
 import CommercialTab from '../../misc/TabContent/OurWork/CommercialTab/CommercialTab';
 import RetailTab from '../../misc/TabContent/OurWork/RetailTab/RetailTab';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProjects } from '../../redux/actions/projectsActions';
 
 const tabContentArray = [
     {
@@ -17,6 +19,15 @@ const tabContentArray = [
 ];
 
 const OurWork = () => {
+    const dispatch = useDispatch();
+    const projects = useSelector(state => state.projects.projects);
+
+    useEffect(() => {
+        dispatch(getProjects());
+        console.log('here');
+    }, [dispatch]);
+
+    console.log(projects)
     return (
         <div>
             <HeroHeader 
