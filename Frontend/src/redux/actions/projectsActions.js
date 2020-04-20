f1135f39776a2af1ae5440d30bf2c90decffa323
japerry911 <jack.e.perry@outlook.com>
2020-04-20 14:03:05 -0600

    ready for merge, and ready for ourworkPage work

--- Frontend/src/redux/actions/projectsActions.js
+++ Frontend/src/redux/actions/projectsActions.js
@@ -2,11 +2,7 @@ import railsServer from '../../api/railsServer';
 
 export const getProjects = () => {
     return async dispatch => {
-        console.log('Fetching!');
         const getProjectsResponse = await railsServer.get('/projects');
-        console.log('Done Fetching');
-        console.log('response below');
-        console.log(getProjectsResponse);
         dispatch({ type: 'GET_PROJECTS', payload: getProjectsResponse.data.projects });
     };
 };
