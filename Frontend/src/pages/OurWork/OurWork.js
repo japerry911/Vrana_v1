7b1b9379cbc44514b7010f56db7cc79950edb0df
japerry911 <jack.e.perry@outlook.com>
2020-04-19 20:40:46 -0600

    Finished Retail Tab

--- Frontend/src/pages/OurWork/OurWork.js
+++ Frontend/src/pages/OurWork/OurWork.js
@@ -4,11 +4,16 @@ import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import HorizontalTabs from '../../components/HorizontalTabs/HoritzontalTabs';
 import Footer from '../../components/Footer/Footer';
 import CommercialTab from '../../misc/TabContent/OurWork/CommercialTab/CommercialTab';
+import RetailTab from '../../misc/TabContent/OurWork/RetailTab/RetailTab';
 
 const tabContentArray = [
     {
         tabTitle: 'Commercial',
         tabContent: <CommercialTab />
+    },
+    {
+        tabTitle: 'Retail',
+        tabContent: <RetailTab />
     }
 ];
 
