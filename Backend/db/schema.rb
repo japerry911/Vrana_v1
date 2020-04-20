dd55973f71203eeca18b619ea224470ff752d8c4
japerry911 <jack.e.perry@outlook.com>
2020-04-19 21:17:06 -0600

    Projects migration created, and default seed set

---
+++ Backend/db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_20_030711) do
+
+  create_table "projects", id: :string, force: :cascade do |t|
+    t.string "Client_Name"
+    t.string "Size"
+    t.string "Location"
+    t.string "YearCompleted_ProjectStatus"
+    t.string "Construction_Value"
+    t.string "Scope_Of_Work"
+    t.string "Industry"
+    t.string "First_P_Header"
+    t.text "First_P_Content"
+    t.text "Key_Projects_Content"
+  end
+
+end
--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+
+Project.create(
+    Client_Name: 'Ameristar',
+    Size: '40,000',
+    Location: 'Council Bluffs, IA',
+    YearCompleted_ProjectStatus: nil,
+    Construction_Value: nil,
+    Scope_Of_Work: nil,
+    Industry: nil,
+    First_P_Header: 'ELEVATING THE CASINO EXPERIENCE',
+    First_P_Content: 'Vrana completed several projects including a Pavilion Renovation, Hotel Lobby, and Main Street Sweet Shoppe.
+    Over 40,000 lf of new wood moldings were install and painted during the renovation of the atrium streetscape. A completely
+    new Hotel Lobby was built within the existing facility. Vrana renovated the existing Players Club counter into the Main Street Sweet Shoppe.',
+    Key_Projects_Content: 'A video wall and sound system was also installed, along with a complete relighting of the Pavilion.'
+)
