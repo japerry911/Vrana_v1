459661238d403327c88d73052b38d81c39a71836
japerry911 <jack.e.perry@outlook.com>
2020-04-20 20:55:42 -0600

    Boilerplate setup - ShowProject page blank created and routed

--- Frontend/src/components/ShowWork/ShowWork.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { useStyles } from './ShowWorkStyles';
-
-const ShowWork = () => {};
-
-export default ShowWork;
---
+++ Frontend/src/pages/ShowProject/ShowProject.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useStyles } from './ShowProjectStyles';
+
+const ShowProject = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <h1>Show Project Page!</h1>
+        </div>
+    );
+};
+
+export default ShowProject;
