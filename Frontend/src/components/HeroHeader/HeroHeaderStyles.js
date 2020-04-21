f2d0dceb21affc1163026bfc9508b00647cf8e12
japerry911 <jack.e.perry@outlook.com>
2020-04-21 17:40:11 -0600

    layout draft complete

--- Frontend/src/components/HeroHeader/HeroHeaderStyles.js
+++ Frontend/src/components/HeroHeader/HeroHeaderStyles.js
@@ -2,7 +2,7 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 
 export const useStyles = makeStyles(theme => ({
     headerDiv: {
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('https://lh3.googleusercontent.com/vC3363SAFXNsR-YU4OAwJejXQnt1FPJpdfaE7I9Wb1_Gu-DkgqcVC_V8daE7am40QtWlY58Pw4AM4qkn830ZBVA3DEZYiFLElDRj6NN5nXunQtb_XxtQIzjFwpmFOMk7F61Ag4dLY1fRbCnWn2o2v2Ewm0fAauLt70Y-FYmSbYOOw8bedSFI_d4PYBsGu_4D7thdVl8gjPuo6THjOXxhUWwPK1mDk-9p8DvFCyGunuWb1cNzkOZONc3MbcTtgLVjFuM8vpEn5iJ8ntisb3J7PBHP_yaVFd2pXnUC7LP0HNq6iNV-8hi6icquOCJeYa9hJdr8IJESBMygM_lhUi46yK9tCscNVbw2vI_tITkTBgpjWNl0Pylww5cLcPunaSoBUsCuisbRD1FArrgS8fIZXboxPf88xc6Jh6CMvpqYtjN3VD8Y3_Ekq5-mPz25QlvlyuaZ-BzzIOdETNzy1F59CNn4x2K426S_aF6kuLERE_EtF6em9u4LFhXFxYTT30VR02KaYskMJK7bc4jnIuocZvgMNBNCwaS-R2fG9j0mZDBkUk7GeSD3vHxKZaF8GIbFXbXWsHx_HeusjwmC192FmEWrzdcydlVW659mNXSknKVdUPIjAlN6OdLLaqnSL-TjHxgctE9ePNG5bVRre2Wyp8yjSfHI8Hn98sYbuVawjoJlzS8pAMRmK0CGYCw-OA=w864-h576-no')`,
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/HW-081616-D-07.jpg')`,
         minHeight: '10em',
         width: '100%',
         backgroundSize: 'cover',
--- Frontend/src/pages/Admin/Admin.js
+++ Frontend/src/pages/Admin/Admin.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { useStyles } from './AdminStyles';
+import Grid from '@material-ui/core/Grid';
+import HeroHeader from '../../components/HeroHeader/HeroHeader';
+import Footer from '../../components/Footer/Footer';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const Admin = () => {
     const classes = useStyles();
 
     return (
         <div>
-            <h1>Admin Page</h1>
+            <HeroHeader />
+            <Grid container spacing={0} className={classes.darkGreyContainerStyle}  justify='center' align='center'>
+                <Grid container spacing={0} className={classes.whiteContainerStyle}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={classes.gridItemStyle} align='center'>
+                        <Typography variant='h4' className={classes.headerFontStyle}>
+                            Sign In
+                        </Typography>
+                        <Divider />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={classes.gridItemStyle} align='center'>
+                        <TextField label='Username' className={classes.textFieldStyle} />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={classes.gridItemStyle} align='center'>
+                        <TextField label='Password' className={classes.textFieldStyle} />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} align='left' className={classes.gridItemStyle} align='center'>
+                        <Button className={classes.buttonStyle}>
+                            <Typography variant='h6' className={classes.buttonTextStyle}>
+                                Sign In
+                            </Typography>
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Footer />
         </div>
     );
 };
