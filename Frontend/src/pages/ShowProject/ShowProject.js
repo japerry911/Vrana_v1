2b124cdee11d72331471697e2a88f46fc6d137fe
japerry911 <jack.e.perry@outlook.com>
2020-04-20 21:39:51 -0600

    added in getProject action call in showProject and tested it successfully

--- Frontend/src/pages/ShowProject/ShowProject.js
+++ Frontend/src/pages/ShowProject/ShowProject.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useStyles } from './ShowProjectStyles';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProject } from '../../redux/actions/projectsActions';
+import Spinner from '../../components/Spinner/Spinner';
 
 const ShowProject = ({ match }) => {
     const classes = useStyles();
     const id = match.params.id;
+    const dispatch = useDispatch();
+    const isLoading = useSelector(state => state.projects.loading);
+    
+    useEffect(() => {
+        dispatch(getProject(id));
+    }, [dispatch]); 
 
     return (
         <div>
-            <h1>Show Project Page!</h1>
+            {isLoading
+            ?
+            <Spinner />
+            :
+            <h1>Show Project Page!</h1>}
         </div>
     );
 };
