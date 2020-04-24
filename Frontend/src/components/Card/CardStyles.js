2a5901c3c955e04f5efdc28ab4dee96ed0fcaaae
japerry911 <jack.e.perry@outlook.com>
2020-04-24 08:38:32 -0600

    Adjusted cardStyles so that they are all same size

--- Frontend/src/components/Card/CardStyles.js
+++ Frontend/src/components/Card/CardStyles.js
@@ -3,14 +3,16 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 export const useStyles = makeStyles(theme => ({
     snapshotImageStyle: {
         borderRadius: 10,
-        width: '50%',
+        width: '30vh',
+        height: '30vh',
+        objectFit: 'cover'
     },
     medGreyHeaderStyle: {
         fontFamily: 'Avenir-Medium',
         color: theme.colors.darkGrey,
         paddingTop: '1em',
         textAlign: props => props.headerAlign ? props.headerAlign : 'left',
-        width: '50%'
+        width: '30vh'
     },
     darkGreyBodyStyle: {
         fontFamily: 'Avenir',
@@ -21,7 +23,7 @@ export const useStyles = makeStyles(theme => ({
         color: theme.colors.vranaRed
     },
     cardTextDivStyle: {
-        width: '50%', 
+        width: '30vh', 
         textAlign: 'left'
     },
     linkStyle: {
