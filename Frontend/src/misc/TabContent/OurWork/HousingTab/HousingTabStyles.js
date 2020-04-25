57b9ee6091194fb4231f67d62d1943bf5ca94cf4
japerry911 <jack.e.perry@outlook.com>
2020-04-25 09:44:22 -0600

    HousingTab component completed on Frontend

---
+++ Frontend/src/misc/TabContent/OurWork/HousingTab/HousingTabStyles.js
@@ -0,0 +1,28 @@
+import makeStyle from '@material-ui/core/styles/makeStyles';
+
+export const useStyles = makeStyles(theme => ({
+    whiteContainerStyle: {
+        backgroundColor: theme.colors.nonTextWhite,
+    },
+    gridRightItemCardStyle: {        
+        marginTop: '3em',
+        paddingLeft: '5em'
+    },
+    gridLeftItemCardStyle: {
+        marginTop: '3em',
+        paddingRight: '5em'
+    },
+    imageBannerGridStyle: {
+        marginTop: '2em'
+    },
+    additionalProjectsTextStyle: {
+        paddingTop: '1em'
+    },
+    redHightlightDivStyle: {
+        fontWeight: 'bold',
+        fontSize: '1.25em',
+        display: 'inline',
+        paddingRight: '.5em',
+        color: theme.colors.vranaRed
+    }
+}));
