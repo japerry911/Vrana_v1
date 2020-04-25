78677ab2b016d21e5f0d1209ba4246818cfa4333
japerry911 <jack.e.perry@outlook.com>
2020-04-25 17:39:38 -0600

    Index route/action added for newsArticle

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :create, :destroy, :update]
+  resources :news_articles, only [:index]
   post '/login', to: 'sessions#login'
 end
