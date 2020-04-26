7d9e68c7764d7047aebfbb927acc4a0d3e53c0a2
japerry911 <jack.e.perry@outlook.com>
2020-04-26 09:22:02 -0600

    Added in spinner, useSelector/useDispatch, and header and basic containers

--- Frontend/src/pages/AddNews/AddNews.js
+++ Frontend/src/pages/AddNews/AddNews.js
@@ -1,12 +1,37 @@
 import React, { Fragment } from 'react';
 import { useStyles } from './AddNewsStyles';
+import HeroHeader from '../../components/HeroHeader/HeroHeader';
+import FormHeader from '../../components/FormHeader/FormHeader';
+import Grid from '@material-ui/core/Grid';
+import Footer from '../../components/Footer/Footer';
+import { useSelector, useDispatch } from 'react-redux';
+import Spinner from '../../components/Spinner/Spinner';
 
 const AddNews = () => {
     const classes = useStyles();
 
+    const isLoading = useSelector(state => state.news.loading);
+    const dispatch = useDispatch();
+
     return (
         <div className={classes.mainDivStyle}>
-            <h1>Add News Page</h1>
+            {isLoading
+            ?
+            <div className={classes.spinnerDiv}>
+                <Spinner />
+            </div>
+            :
+            <Fragment>
+                <HeroHeader headerText='Admin: Add News' />
+                <Grid container spacing={0} className={classes.darkGreyContainerStyle}  justify='center' align='center' item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <form className={classes.formContainerStyle}>
+                        <Grid container spacing={0} className={classes.whiteContainerStyle}>
+                            <FormHeader headerText='Add News' />
+                        </Grid>
+                    </form>
+                </Grid>
+                <Footer />
+            </Fragment>}
         </div>
     );
 };
