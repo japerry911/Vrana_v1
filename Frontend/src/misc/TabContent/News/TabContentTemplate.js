2a24a0fe7fa90ed04a173d7b0bb36de608e8598e
japerry911 <jack.e.perry@outlook.com>
2020-04-25 18:45:48 -0600

    Boilerplate news and tabcontenttemplate news components

---
+++ Frontend/src/misc/TabContent/News/TabContentTemplate.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { useStyles } from './TabContentTemplateStyles';
+
+const TabContentTemplate = ({ tabContent }) => {
+    const classes = useStyles();
+
+    return (
+        <Fragment>
+        </Fragment>
+    );
+};
+
+export default TabContentTemplate;
--- Frontend/src/pages/News/News.js
+++ Frontend/src/pages/News/News.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './News.css';
+import React, { useEffect } from 'react';
+import { useStyles } from './NewsStyles.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { getNewsArticles } from '../../redux/actions/newsActions';
 
 const News = () => {
+    const classes = useStyles();
+
+    const isLoading = useSelector(state => state.news.loading);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getNewsArticles());
+    }, [dispatch]);
+
     return (
         <div>
-            <h1>News</h1>
+            <h1>News Page</h1>
         </div>
     );
 };
