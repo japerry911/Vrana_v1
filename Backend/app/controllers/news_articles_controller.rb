9fadcbc6ac70affa6725c59abf67906e72fb7833
japerry911 <jack.e.perry@outlook.com>
2020-04-26 09:10:57 -0600

    Backend setup completed

--- Backend/app/controllers/news_articles_controller.rb
+++ Backend/app/controllers/news_articles_controller.rb
@@ -4,4 +4,30 @@ class NewsArticlesController < ApplicationController
 
         render json: { articles: @news_articles }
     end
+
+    def create
+        strong_params = article_params_exc_image_urls
+
+        s3 = Aws::S3::Resource.new 
+        image_url_base = 'https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/News_Articles'
+
+        image_s3_path = s3.bucket('vranaconstructionwebsiteimages')
+                .object("News_Articles/#{strong_params[:image_filename]}.#{strong_params[:image_filetype]}")
+        image_status = image_s3_path.upload_file(strong_params[:image])
+        if !image_status
+            render status: :internal_server_error
+        end
+        image_url = "#{image_url_base}/#{strong_params[:image_filename]}.#{strong_params[:image_filetype]}"
+
+        @new_article = NewsArticle.create([Headline: strong_params[:Headline], Source: strong_params[:Source], Date_Published: strong_params[:Date_Published],
+                                            Article_Link: strong_params[:Article_Link], Feature_Image_Url: image_url])
+        
+        render json: { article: @new_article }
+    end
+
+    private
+    
+        def article_params_exc_image_urls
+            params.permit([:Headline, :Source, :Date_Published, :Article_Link, :image_filetype, :image, :image_filename])
+        end
 end
--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :create, :destroy, :update]
-  resources :news_articles, only: [:index]
+  resources :news_articles, only: [:index, :create]
   post '/login', to: 'sessions#login'
 end
