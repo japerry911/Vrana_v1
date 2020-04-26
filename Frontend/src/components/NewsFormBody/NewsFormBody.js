c7185d250e4a12290e9c27f18cf8b292a51b2d68
japerry911 <jack.e.perry@outlook.com>
2020-04-26 09:37:11 -0600

    Form layout setup || added in grids to ProjectFormBody component || integrated useFormFields into AddNews

---
+++ Frontend/src/components/NewsFormBody/NewsFormBody.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react';
+import FormTextField from '../FormTextField/FormTextField';
+import FormImageUploader from '../FormImageUploader/FormImageUploader';
+import Grid from '@material-ui/core/Grid';
+
+const NewsFormBody = ({ fields, setField, setImageField }) => {
+    return (
+        <Fragment>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <FormTextField 
+                paddingTop='1em'
+                label='Headline' 
+                value={fields.headline}
+                onChange={setField}
+                id='headline'
+            />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <FormTextField
+                    label='Source' 
+                    value={fields.source}
+                    onChange={setField}
+                    id='source'
+                />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <FormTextField
+                    label='Date Published (yyyy-MM-dd)' 
+                    value={fields.datePublished}
+                    onChange={setField}
+                    id='datePublished'
+                />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <FormTextField
+                    label='Article URL' 
+                    value={fields.articleLink}
+                    onChange={setField}
+                    id='articleLink'
+                />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <FormImageUploader
+                    onChange={setImageField}
+                    emptyField={fields.newsImage === ''}
+                    labelText='Upload Article Image'
+                    id='newsImage'
+                />
+            </Grid>
+        </Fragment>
+    );
+};
+
+export default NewsFormBody;
--- Frontend/src/pages/AddNews/AddNews.js
+++ Frontend/src/pages/AddNews/AddNews.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useStyles } from './AddNewsStyles';
 import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import FormHeader from '../../components/FormHeader/FormHeader';
@@ -6,9 +6,23 @@ import Grid from '@material-ui/core/Grid';
 import Footer from '../../components/Footer/Footer';
 import { useSelector, useDispatch } from 'react-redux';
 import Spinner from '../../components/Spinner/Spinner';
+import NewsFormBody from '../../components/NewsFormBody/NewsFormBody';
+import { useFormFields } from '../../hooks/customHooks';
+import FormButton from '../../components/FormButton/FormButton';
+
+const INITIAL_STATE = {
+    headline: '',
+    source: '',
+    datePublished: '',
+    articleLink: '',
+    newsImage: ''
+}
 
 const AddNews = () => {
     const classes = useStyles();
+
+    const [fields, setField, setImageField, setFields] = useFormFields(INITIAL_STATE);
+    const [validationStatus, setValidationStatus] = useState(false);
 
     const isLoading = useSelector(state => state.news.loading);
     const dispatch = useDispatch();
@@ -26,7 +40,16 @@ const AddNews = () => {
                 <Grid container spacing={0} className={classes.darkGreyContainerStyle}  justify='center' align='center' item xs={12} sm={12} md={12} lg={12} xl={12}>
                     <form className={classes.formContainerStyle}>
                         <Grid container spacing={0} className={classes.whiteContainerStyle}>
-                            <FormHeader headerText='Add News' />
+                            <FormHeader headerText='Add News Article' />
+                            <NewsFormBody
+                                fields={fields}
+                                setField={setField}
+                                setImageField={setImageField}
+                            />
+                            <FormButton 
+                                buttonText={'Add News Article'}
+                                validationStatus={validationStatus}
+                            />
                         </Grid>
                     </form>
                 </Grid>
