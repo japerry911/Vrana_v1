8135a0d658bc5ad825e3747987b94ad9a725d077
japerry911 <jack.e.perry@outlook.com>
2020-04-26 19:48:25 -0600

    backend showEquipment configured

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :create, :destroy, :update]
   resources :news_articles, only: [:index, :create, :destroy, :update]
-  resources :equipment, only: [:index]
+  resources :equipment, only: [:index, :show]
   post '/login', to: 'sessions#login'
 end
