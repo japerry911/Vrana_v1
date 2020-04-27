92b04e4e91f78f248106b51ce4cfa558779f0c79
japerry911 <jack.e.perry@outlook.com>
2020-04-26 22:47:01 -0600

    signing off committing whats done, started on actual AddEquipment component

--- Frontend/src/pages/AddEquipment/AddEquipment.js
+++ Frontend/src/pages/AddEquipment/AddEquipment.js
@@ -1,8 +1,32 @@
 import React, { Fragment } from 'react';
 import { useStyles } from './AddEquipmentStyles';
+import HeroHeader from '../../components/HeroHeader/HeroHeader';
+import FormHeader from '../../components/FormHeader/FormHeader';
+import Grid from '@material-ui/core/Grid';
+import Footer from '../../components/Footer/Footer';
+import { useSelector, useDispatch } from 'react-redux';
+import Spinner from '../../components/Spinner/Spinner';
+import { useFormFields } from '../../hooks/customHooks';
+import FormButton from '../../components/FormButton/FormButton';
+import { createEquipment } from '../../redux/actions/equipmentActions'
+
+const INITIAL_STATE = {
+    name: '',
+    price: 0.0,
+    year: '',
+    description: '',
+    cardImage: '',
+    leftImage: '',
+    rightImage: ''
+}
 
 const AddEquipment = () => {
     const classes = useStyles();
+
+    const [fields, setField, setImageField, setFields] = useFormFields(INITIAL_STATE);
+
+    const isLoading = useSelector(state => state.equipment.loading);
+    const dispatch = dispatch();
 
     return (
         <div className={classes.mainDivStyle}>
