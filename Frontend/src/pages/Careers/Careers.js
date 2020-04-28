32421527976888023dc9ce832ed690af9a8471c2
japerry911 <jack.e.perry@outlook.com>
2020-04-27 21:36:56 -0600

    Boilerplate career setup

--- Frontend/src/pages/Careers/Careers.js
+++ Frontend/src/pages/Careers/Careers.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import './Careers.css';
+import { useStyles } from './CareersStyles';
 
 const Careers = () => {
+    const classes = useStyles();
+
     return (
         <div>
             <h1>Careers</h1>
