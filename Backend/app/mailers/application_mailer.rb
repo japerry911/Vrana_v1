2d128a0a0e50fa8bcf6b13eda8bf8dd990d94844
japerry911 <jack.e.perry@outlook.com>
2020-04-29 06:31:14 -0600

    fixed wrong env username email env var

--- Backend/app/mailers/application_mailer.rb
+++ Backend/app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['RAILS_ENV'] == 'production' ? ENV['user_name'] : Rails.application.credentials.gmail[:user_name]
-  default to: ENV['RAILS_ENV'] == 'production' ? ENV['user_name'] : Rails.application.credentials.gmail[:user_name]
+  default from: ENV['RAILS_ENV'] == 'production' ? ENV['EMAIL_USERNAME'] : Rails.application.credentials.gmail[:user_name]
+  default to: ENV['RAILS_ENV'] == 'production' ? ENV['EMAIL_USERNAME'] : Rails.application.credentials.gmail[:user_name]
   layout 'mailer'
 end
