cc8fa4f3166817d35e3b2af6b1540c78d615ea18
japerry911 <jack.e.perry@outlook.com>
2020-04-28 22:09:30 -0600

    Deployment ready - DO-NOT-MERGE

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -11,7 +11,7 @@ import thunkMiddleware from 'redux-thunk';
 
 const store = createStore(
   allReducers,
-  compose(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(thunkMiddleware))//, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 ReactDOM.render(
