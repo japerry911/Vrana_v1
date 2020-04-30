33641b633e3b336d36d932accd5a69fd75052aed
japerry911 <jack.e.perry@outlook.com>
2020-04-30 14:06:32 -0600

    page made responsive, ready-for-merge

--- Frontend/src/pages/About/About.js
+++ Frontend/src/pages/About/About.js
@@ -14,9 +14,9 @@ const About = () => {
         <Fragment>
             <HeroHeader headerText='ABOUT US' />
             <Grid container spacing={0} className={classes.whiteContainerStyle} alignItems='center' justify='center'>
-                <Grid item xs={8} sm={8} md={8} lg={8} xl={8} className={classes.aboutUsGreetingContentGridStyle}>
+                <Grid item xs={12} sm={12} md={8} lg={8} xl={8} className={classes.aboutUsGreetingContentGridStyle}>
                     <div className={classes.charIntComHeaderDivStyle}>
-                        <Typography variant='h5' className={classes.blackHeaderTextStyle}>
+                        <Typography variant='h6' className={classes.blackHeaderTextStyle}>
                             CHARACTER.&nbsp;
                             <span className={classes.redHeaderTextStyle}>
                                 INTEGRITY.&nbsp;
@@ -37,7 +37,7 @@ const About = () => {
                         of quality construction and economy. 
                     </Typography>
                 </Grid>
-                <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
                     <img
                         alt='Charles Vrana and Troy Perry'
                         src='https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-04-17+at+11.png'
@@ -61,7 +61,7 @@ const About = () => {
                         dividerColor='dark'
                     />
                 </Grid>
-                <Grid item xs={3} sm={3} md={3} lg={3} xl={3} align='center'>
+                <Grid item xs={12} sm={12} md={6} lg={3} xl={3} align='center'>
                     <img
                         alt='Troy - CEO & President'
                         src='https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/IMG_5465.jpeg'
@@ -74,7 +74,7 @@ const About = () => {
                         CEO & President 
                     </Typography>
                 </Grid>
-                <Grid item xs={3} sm={3} md={3} lg={3} xl={3} align='center'>
+                <Grid item xs={12} sm={12} md={6} lg={3} xl={3} align='center'>
                     <img
                         alt='Charles Vrana - President'
                         src='https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-04-18+at+2.01.51+PM.jpg'
@@ -87,7 +87,7 @@ const About = () => {
                         President 
                     </Typography>
                 </Grid>
-                <Grid item xs={3} sm={3} md={3} lg={3} xl={3} align='center'>
+                <Grid item xs={12} sm={12} md={6} lg={3} xl={3} align='center'>
                     <img
                         alt='Jeff - Controller'
                         src='https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-04-18+at+2.02.14+PM.jpg'
@@ -100,7 +100,7 @@ const About = () => {
                         Controller
                     </Typography>
                 </Grid>
-                <Grid item xs={3} sm={3} md={3} lg={3} xl={3} align='center'>
+                <Grid item xs={12} sm={12} md={6} lg={3} xl={3} align='center'>
                     <img
                         alt='John - Director of Preconstruction'
                         src='https://vranaconstructionwebsiteimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-04-18+at+2.02.40+PM.jpg'
--- Frontend/src/pages/About/AboutStyles.js
+++ Frontend/src/pages/About/AboutStyles.js
@@ -1,14 +1,6 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
 export const useStyles = makeStyles(theme => ({
-    aboutUsHeaderDiv: {
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url('https://lh3.googleusercontent.com/vC3363SAFXNsR-YU4OAwJejXQnt1FPJpdfaE7I9Wb1_Gu-DkgqcVC_V8daE7am40QtWlY58Pw4AM4qkn830ZBVA3DEZYiFLElDRj6NN5nXunQtb_XxtQIzjFwpmFOMk7F61Ag4dLY1fRbCnWn2o2v2Ewm0fAauLt70Y-FYmSbYOOw8bedSFI_d4PYBsGu_4D7thdVl8gjPuo6THjOXxhUWwPK1mDk-9p8DvFCyGunuWb1cNzkOZONc3MbcTtgLVjFuM8vpEn5iJ8ntisb3J7PBHP_yaVFd2pXnUC7LP0HNq6iNV-8hi6icquOCJeYa9hJdr8IJESBMygM_lhUi46yK9tCscNVbw2vI_tITkTBgpjWNl0Pylww5cLcPunaSoBUsCuisbRD1FArrgS8fIZXboxPf88xc6Jh6CMvpqYtjN3VD8Y3_Ekq5-mPz25QlvlyuaZ-BzzIOdETNzy1F59CNn4x2K426S_aF6kuLERE_EtF6em9u4LFhXFxYTT30VR02KaYskMJK7bc4jnIuocZvgMNBNCwaS-R2fG9j0mZDBkUk7GeSD3vHxKZaF8GIbFXbXWsHx_HeusjwmC192FmEWrzdcydlVW659mNXSknKVdUPIjAlN6OdLLaqnSL-TjHxgctE9ePNG5bVRre2Wyp8yjSfHI8Hn98sYbuVawjoJlzS8pAMRmK0CGYCw-OA=w864-h576-no')`,
-        minHeight: '10em',
-        width: '100%',
-        backgroundSize: 'cover',
-        paddingBottom: '5em',
-        backgroundPosition: '0 33%'
-    },
     aboutUsHeaderTextStyle: {
         fontFamily: 'Avenir-Bold',
         color: theme.colors.textWhite,
@@ -16,13 +8,15 @@ export const useStyles = makeStyles(theme => ({
         marginTop: '5%'
     },
     aboutUsGreetingContentGridStyle: {
-        maxWidth: '45%',
-        paddingRight: '5%'
+        paddingRight: '5%',
+        maxWidth: '45%', [theme.breakpoints.down('sm')]: { maxWidth: '85%' }
     },
     charlesAndTroyImageStyle: {
         paddingTop: '5%',
-        paddingLeft: '5%',
-        borderRadius: 10
+        paddingLeft: '8%',
+        borderRadius: 10,
+        height: 'auto',
+        width: '80%'
     },
     blackHeaderTextStyle: {
         fontFamily: 'Avenir-Bold',
@@ -31,7 +25,7 @@ export const useStyles = makeStyles(theme => ({
     redHeaderTextStyle: {
         fontFamily: 'Avenir-Bold',
         color: theme.colors.vranaRed,
-        display: 'inline'
+        display: 'inline-block'
     },
     charIntComHeaderDivStyle: {
         paddingBottom: '3%'
@@ -51,7 +45,7 @@ export const useStyles = makeStyles(theme => ({
     profileImageStyle: {
         borderRadius: 10,
         height: 400,
-        width: 'auto'
+        width: 'auto', [theme.breakpoints.down('sm')]: { height: 300 }
     },
     profileTitleStyle: {
         color: theme.colors.darkGrey,
